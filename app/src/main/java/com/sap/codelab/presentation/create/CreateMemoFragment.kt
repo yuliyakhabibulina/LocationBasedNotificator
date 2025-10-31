3d543a692c376145297eb9a8f5eefe1632604e0b
Julia Khabibulina <yuliya.khabibulina@innowise.com>
2025-10-31 08:42:46 +0100

    Added permission checks

--- app/src/main/java/com/sap/codelab/presentation/create/CreateMemoFragment.kt
+++ app/src/main/java/com/sap/codelab/presentation/create/CreateMemoFragment.kt
@@ -1,8 +1,10 @@
 package com.sap.codelab.presentation.create
 
+import android.os.Build
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -12,6 +14,12 @@ import com.sap.codelab.R
 import com.sap.codelab.databinding.FragmentCreateMemoBinding
 import com.sap.codelab.utils.extensions.collectFlow
 import dagger.hilt.android.AndroidEntryPoint
+import android.Manifest
+import android.content.Intent
+import android.provider.Settings
+import android.net.Uri
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 
 @AndroidEntryPoint
 class CreateMemoFragment : Fragment() {
@@ -21,6 +29,32 @@ class CreateMemoFragment : Fragment() {
 
     private val viewModel: CreateMemoViewModel by viewModels()
 
+    private val permissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val allGranted = permissions.entries.all { it.value }
+            if (allGranted) {
+                navigateToMapFragment()
+            } else {
+                if (shouldShowSettingsDialog()) {
+                    showSettingsDialog()
+                } else {
+                    Toast.makeText(requireContext(), getString(R.string.permissions_not_granted), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    private val requiredPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.POST_NOTIFICATIONS
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -43,7 +77,7 @@ class CreateMemoFragment : Fragment() {
 
     private fun setupListeners() = with(binding){
         buttonGetLocation.setOnClickListener {
-            findNavController().navigate(R.id.action_createMemoFragment_to_mapFragment)
+            handlePermissionsRequest()
         }
 
 //        setFragmentResultListener(MapFragment.REQUEST_KEY_LOCATION) { _, bundle ->
@@ -94,5 +128,51 @@ class CreateMemoFragment : Fragment() {
             viewLifecycleOwner,
             Lifecycle.State.RESUMED
         )
+    }
+    private fun handlePermissionsRequest() {
+        if (shouldShowRationaleDialog()) {
+            showRationaleDialog()
+        } else {
+            permissionsLauncher.launch(requiredPermissions)
+        }
+    }
+
+    private fun shouldShowRationaleDialog(): Boolean {
+         return requiredPermissions.any {
+            ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), it)
+        }
+    }
+
+    private fun shouldShowSettingsDialog(): Boolean {
+          return !shouldShowRationaleDialog()
+    }
+
+    private fun showRationaleDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle(R.string.permission_dialog_rationale_title)
+            .setMessage(R.string.permission_dialog_rationale_message)
+            .setPositiveButton(R.string.permission_dialog_rationale_positive_button) { _, _ ->
+                permissionsLauncher.launch(requiredPermissions)
+            }
+            .setNegativeButton(R.string.permission_dialog_rationale_negative_button, null)
+            .show()
+    }
+
+    private fun showSettingsDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle(R.string.permission_dialog_settings_title)
+            .setMessage(R.string.permission_dialog_settings_message)
+            .setPositiveButton(R.string.permission_dialog_settings_positive_button) { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", requireActivity().packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton(R.string.permission_dialog_settings_negative_button, null)
+            .show()
+    }
+
+    private fun navigateToMapFragment() {
+        findNavController().navigate(R.id.action_createMemoFragment_to_mapFragment)
     }
 }
